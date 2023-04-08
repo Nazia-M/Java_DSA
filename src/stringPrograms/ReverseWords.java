c8a55df03e98a50256422d33a2aa402c8e0e33d1
Nazia <nazia862@gmail.com>
2023-04-08 17:40:08 +0530

    Prog

--- src/stringPrograms/ReverseWords.java
+++ src/stringPrograms/ReverseWords.java
@@ -1,4 +1,23 @@
 package stringPrograms;
 
 public class ReverseWords {
+
+    public static void main(String[] args) {
+
+        String str = "Java is very Easy";
+
+        String words[] = str.split("\\s");
+
+        String revStr = "";
+
+        for(int i=0;i<words.length;i++){
+
+            if(i == words.length-1)
+                    revStr = words[i] + revStr;
+            else
+                revStr = " "+ words[i] + revStr;
+        }
+        System.out.println(revStr);
+
+    }
 }
