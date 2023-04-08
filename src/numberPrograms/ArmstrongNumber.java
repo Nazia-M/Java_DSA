6fa68e41175acbb715e8716449960a5855826841
Nazia <nazia862@gmail.com>
2023-04-08 19:16:53 +0530

    Prog

--- src/numberPrograms/ArmstrongNumber.java
+++ src/numberPrograms/ArmstrongNumber.java
@@ -1,4 +1,40 @@
 package numberPrograms;
 
 public class ArmstrongNumber {
+
+    public static void main(String[] args) {
+
+//        153 = 1*1*1 + 5*5*5 + 3*3*3 = 1 + 125 + 27 = 153
+//        1634 = 1*1*1*1 + 6*6*6*6 + 3*3*3*3 + 4*4*4*4 = 1 + 1296 + 81 + 256 = 1634
+
+        int n=1634, mul=0,a,temp, sum=0;
+        int len=0, num=n;
+
+        System.out.println("num "+num);
+        while(num>0)
+        {
+            num=num/10;
+            len = len+1;
+        }
+        System.out.println("len "+len);
+
+        temp=n;
+        while(n>0)
+        {
+            a=n%10;
+            System.out.print(a+" ");
+            n=n/10;
+            for(int i=1;i<len-1;i++){
+                a = a*a;
+            }
+            System.out.println("Multiplies a : "+a);
+            sum = sum+ a;
+            System.out.println(sum+" ");
+        }
+        if(temp==sum)
+            System.out.println("Armstrong number");
+        else
+            System.out.println("Not Armstrong number");
+
+    }
 }
--- src/stringPrograms/RemoveExtraWhitespaces.java
+++ src/stringPrograms/RemoveExtraWhitespaces.java
@@ -3,10 +3,24 @@ package stringPrograms;
 public class RemoveExtraWhitespaces {
     public static void main(String[] args) {
 
+        //Implementation 1 : Using replaceAll()
         String str = "how to   do    in  java.  com";
-
         String newStr = str.replaceAll("\\s+"," ");
+        System.out.println("Using replaceAll() : "+newStr);
 
-        System.out.println(newStr);
+        //Implementation 2 : Using StringBuffer()
+        String str1 = "how to   do    in  java.  com";
+        char[] chars = str1.toCharArray();
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i<chars.length; i++)
+        {
+            if( (chars[i] != ' ') && (chars[i] != '\t') )
+            {
+                sb.append(chars[i]);
+            }
+        }
+        System.out.println("Using StringBuffer() : "+sb);
+
+
     }
 }
