9c5305b2656b4646d328f627d63d66cd457a3e0f
Nazia <nazia862@gmail.com>
2023-04-08 17:44:10 +0530

    Prog

--- src/stringPrograms/CountVowelsAndConsonants.java
+++ src/stringPrograms/CountVowelsAndConsonants.java
@@ -1,4 +1,24 @@
 package stringPrograms;
 
 public class CountVowelsAndConsonants {
+    public static void main(String[] args) {
+
+        String str = "Java Programming";
+        str = str.toLowerCase();
+        int vowels = 0, cons =0;
+
+        for(int i=0;i< str.length();i++){
+
+            if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u') {
+                vowels++;
+            } else if (str.charAt(i) >= 'a' && str.charAt(i) <= 'z')
+            {
+                cons++;
+            }
+        }
+
+        System.out.println("Number of vowels: " + vowels);
+        System.out.println("Number of consonants: " + cons);
+
+    }
 }
