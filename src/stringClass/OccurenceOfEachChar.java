0139641cf9bc5150e8fee1c8f8c4090fb8d031fd
Nazia <nazia862@gmail.com>
2023-04-08 17:14:30 +0530

    Prog

--- src/stringClass/OccurenceOfEachChar.java
+++ src/stringClass/OccurenceOfEachChar.java
@@ -1,4 +1,41 @@
 package stringClass;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.SortedMap;
+
 public class OccurenceOfEachChar {
+
+    public static void main(String[] args) {
+
+        String str = "iamharry";
+
+        char arr[] = str.toCharArray();
+
+        //HashMap gives different outcome every time you run
+        //LinkedHashMap - Maintains insertion order
+        LinkedHashMap<Character,Integer> map = new LinkedHashMap<>();
+
+        for(int i=0;i<arr.length;i++){
+            char ch = arr[i];
+            if(map.containsKey(ch))
+                map.put(ch,map.get(ch)+1);
+            else
+                map.put(ch,1);
+        }
+
+        Iterator<Character> it = map.keySet().iterator();
+        while(it.hasNext())
+        {
+            char key = it.next();
+            int value = map.get(key);
+            System.out.println("Key : "+key+" value : "+value);
+        }
+
+
+
+
+
+    }
 }
