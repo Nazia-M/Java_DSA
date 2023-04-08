b2e505a874314b95f94f2797d72a360613caf6d1
Nazia <nazia862@gmail.com>
2023-04-08 17:56:23 +0530

    Prog

--- src/numberPrograms/FibonacciSeries.java
+++ src/numberPrograms/FibonacciSeries.java
@@ -1,4 +1,21 @@
 package numberPrograms;
 
 public class FibonacciSeries {
+
+    public static void main(String[] args) {
+
+        int num=10, n1=0, n2=1, counter=0;
+
+        while(counter < num){
+
+            System.out.print(n1+" ");
+            int n3 = n2+n1;
+            n1 = n2;
+            n2 = n3;
+            counter = counter+1;
+
+        }
+
+
+    }
 }
