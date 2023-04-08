8b418ef714ef21c210f7b4462deef60d2821fc49
Nazia <nazia862@gmail.com>
2023-04-08 17:45:54 +0530

    Prog

--- src/stringPrograms/RemoveExtraWhitespaces.java
+++ src/stringPrograms/RemoveExtraWhitespaces.java
@@ -1,4 +1,12 @@
 package stringPrograms;
 
 public class RemoveExtraWhitespaces {
+    public static void main(String[] args) {
+
+        String str = "how to   do    in  java.  com";
+
+        String newStr = str.replaceAll("\\s+"," ");
+
+        System.out.println(newStr);
+    }
 }
