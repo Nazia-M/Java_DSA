4d90e89fe2e74c8ef46ea076e39df37201659eb2
Nazia <nazia862@gmail.com>
2023-04-08 17:51:44 +0530

    Prog

--- src/numberPrograms/SwapNumWithoutThirdvar.java
+++ src/numberPrograms/SwapNumWithoutThirdvar.java
@@ -1,4 +1,16 @@
 package numberPrograms;
 
 public class SwapNumWithoutThirdvar {
+
+    public static void main(String[] args) {
+
+        int x=5, y=10;
+
+        x=x+y;
+        y=x-y;
+        x=x-y;
+
+        System.out.println("x : "+x+" y : "+y);
+
+    }
 }
