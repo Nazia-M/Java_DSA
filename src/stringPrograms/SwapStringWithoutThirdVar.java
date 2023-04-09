f46126a6e289a0f39b93ec835d07f9fbd6fabde7
Nazia <nazia862@gmail.com>
2023-04-09 14:54:58 +0530

    prep

--- src/stringPrograms/SwapStringWithoutThirdVar.java
+++ src/stringPrograms/SwapStringWithoutThirdVar.java
@@ -1,4 +1,19 @@
 package stringPrograms;
 
 public class SwapStringWithoutThirdVar {
+
+    public static void main(String[] args) {
+
+        String a = "Hello", b = "World";
+
+        System.out.println(a+" "+b);
+
+        a = a+b;
+        b = a.substring(0, a.length()-b.length());
+        a = a.substring(b.length());
+
+        System.out.println("After swapping : "+a+" "+b);
+
+
+    }
 }
