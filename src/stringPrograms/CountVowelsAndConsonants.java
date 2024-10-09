35a80cd2c107e3ada55b43d01f78cc1b3ccdb4a3
Nazia <nazia862@gmail.com>
2024-10-09 06:44:37 +0530

    Commit 9 OCT 2024

--- src/stringPrograms/CountVowelsAndConsonants.java
+++ src/stringPrograms/CountVowelsAndConsonants.java
@@ -9,7 +9,7 @@ public class CountVowelsAndConsonants {
 
         for(int i=0;i< str.length();i++){
 
-            if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u'  || str.charAt(i) == 'A') {
+            if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u'  || str.charAt(i) == 'A'   || str.charAt(i) == 'E') {
                 vowels++;
             } else if (str.charAt(i) >= 'a' && str.charAt(i) <= 'z')
             {
