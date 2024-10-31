e31bfa1c332a132bbebb3ebbb08bc03afdc80db7
Nazia <nazia862@gmail.com>
2024-10-31 08:17:01 +0530

    Commit 9 OCT 2024

--- src/playGround/playGround1.java
+++ src/playGround/playGround1.java
@@ -16,25 +16,40 @@ public class playGround1 {
 //        System.out.println("Last digit : "+(n%10));
 
         //Reverse a number, number cna be negative
-        int num = -523423;
-        boolean negative = false;
-        int lastDigit =0, revNum = 0;
+//        int num = -523423;
+//        boolean negative = false;
+//        int lastDigit =0, revNum = 0;
+//
+//        if(num <0 ){
+//            negative = true;
+//            num = Math.abs(num);
+//        }
+//
+//        while(num > 0){
+//            lastDigit = num%10;
+//            revNum = (revNum * 10)+lastDigit;
+//            num = num/10;
+//        }
+//
+//        System.out.println("RevNumber "+revNum);
+//
+//        revNum = ~(revNum-1);
+//        System.out.println("RevNumber "+revNum);
 
-        if(num <0 ){
-            negative = true;
-            num = Math.abs(num);
+        //Swap 2 numbers without third number
+//        int a = 3, b=5;
+//        a=a+b;
+//        b=a-b;
+//        a=a-b;
+//        System.out.println("a value "+a);
+//        System.out.println("b value "+b);
+
+        //Print even numbers between 1 to 10
+        for( int i=1;i<=100;i++){
+            if(i%2 == 0)
+                System.out.println("Even no : "+i);
         }
 
-        while(num > 0){
-            lastDigit = num%10;
-            revNum = (revNum * 10)+lastDigit;
-            num = num/10;
-        }
-
-        System.out.println("RevNumber "+revNum);
-
-        revNum = ~(revNum-1);
-        System.out.println("RevNumber "+revNum);
 
 
 
