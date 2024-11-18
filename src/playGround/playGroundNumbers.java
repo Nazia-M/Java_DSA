e170989e4f8067b2f2db72ab2a43b7ab379fe2e3
Nazia <nazia862@gmail.com>
2024-11-18 09:37:49 +0530

    Commit 9 OCT 2024

---
+++ src/playGround/playGroundNumbers.java
@@ -0,0 +1,71 @@
+package playGround;
+
+public class playGroundNumbers {
+    public static void main(String[] args) {
+
+        //First and last digit
+//        int num = 656456454, ld=0;
+//
+//        System.out.println("Last Digit : "+(num%10));
+//
+//        while(num != 0){
+//
+//            ld = num%10;
+//            num = num/10;
+//        }
+//        System.out.println("First Digit : "+ld);
+
+        //Remove last digit
+//        int num = 4342, ld =0;
+//
+//        ld = num%10;
+//        num = num/10;
+//
+//        System.out.println("Num "+num);
+
+        //Reverse Number
+//        int n= -52433532,ld=0, rev=0;
+//        boolean neg = false;
+//
+//        if(n <0){
+//            neg = true;
+//            n = Math.abs(n);
+//        }
+//        while(n != 0){
+//
+//            ld = n%10;
+//            rev = (rev*10)+ld;
+//            n = n/10;
+//        }
+//        System.out.println("Reverse is"+rev);
+//
+//        if(neg == true);
+//        {
+//            rev = ~(rev-1);
+//        }
+//        System.out.println("Neg "+rev);
+
+
+        //Swap 2 numbers without 3rd number
+//        int a = 5, b=3;
+//
+//        a = a+b;
+//        b = a-b;
+//        a = a-b;
+//
+//        System.out.println("a "+a+" b "+b);
+
+
+        //Print evenn numbers 1 to 10
+//        for(int i=1;i<=10;i++){
+//            if(i%2 == 0)
+//                System.out.println("Even "+i);
+//        }
+
+
+
+
+
+
+    }
+}
--- src/stringPrograms/stringAnagram.java
+++ src/stringPrograms/stringAnagram.java
@@ -1,5 +1,30 @@
 package stringPrograms;
 
+import java.util.HashMap;
+import java.util.Iterator;
+
 public class stringAnagram {
 
+    public static <Char> void main(String[] args) {
+
+        String str = "tell", str1 = "ltel";
+
+        int len = str.length(), len1= str1.length();
+
+        if(len == len1){
+
+            char ch[] = str.toCharArray();
+            char ch1[] = str1.toCharArray();
+
+
+
+            for(int i=0;i<ch.length;i++){
+
+
+
+            }
+
+        }
+
+    }
 }
