d1084878d8d0013166e9a5912038d5e9d49304d1
Nazia <nazia862@gmail.com>
2024-11-20 08:46:37 +0530

    Commit 9 OCT 2024

--- src/patternPrograms/RightTraingleStart.java
+++ src/patternPrograms/RightTraingleStart.java
@@ -2,11 +2,56 @@ package patternPrograms;
 
 public class RightTraingleStart {
     public static void main(String[] args) {
-        for(int i=1;i<=4;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print("* ");
-            }
-            System.out.print("\n");
-        }
+
+
+//        for(int i=1;i<=4;i++){
+//            for(int j=1;j<=i;j++){
+//                System.out.print("* ");
+//            }
+//            System.out.print("\n");
+//        }
+
+
+//        for(int i=1;i<=4;i++){
+//            for(int j=4;j>=i;j--){
+//                System.out.print("* ");
+//            }
+//            System.out.print("\n");
+//        }
+//
+//        for(int i=4; i>=1; i--)       //Rows
+//        {
+//            for(int j=1 ;j<=i; j++)      //Columns
+//            {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+//        for(int i=1;i<=4;i++){
+//            for(int j=1;j<=i;j++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+//
+//        for(int i=1; i<=4; i++)
+//        {
+//            for(int j=4 ;j>=i; j--)
+//            {
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+
+
+
+
+
+
+
+
+
+
     }
 }
