16d51f764c3075b7e2930c0f61ccdf23b29b0e44
Nazia <nazia862@gmail.com>
2024-11-21 21:19:51 +0530

    Commit 9 OCT 2024

--- src/patternPrograms/RightTraingleStart.java
+++ src/patternPrograms/RightTraingleStart.java
@@ -110,16 +110,16 @@ public class RightTraingleStart {
 //        }
 
 
-//        for(int i=1;i<=4;i++){
-//
-//            for(int j=4;j>=i;j--){
-//                System.out.print(" ");
-//            }
-//            for(int k=1;k<(i*2);k++){
-//                System.out.print("*");
-//            }
-//            System.out.println();
-//        }
+        for(int i=1;i<=4;i++){
+
+            for(int j=4;j>=i;j--){
+                System.out.print(" ");
+            }
+            for(int k=1;k<(i*2);k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
 
         for(int i=5;i>=1;i--){
