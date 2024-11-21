4bff3e7e77a0b2f4273d20ebe73324e57b735a33
Nazia <nazia862@gmail.com>
2024-11-21 09:06:24 +0530

    Commit 9 OCT 2024

--- src/patternPrograms/RightTraingleStart.java
+++ src/patternPrograms/RightTraingleStart.java
@@ -76,28 +76,62 @@ public class RightTraingleStart {
 
 
 
-        for(int i=1;i<=4;i++){
-            for(int j=3;j>=i;j--){
+//        for(int i=1;i<=4;i++){
+//            for(int j=3;j>=i;j--){
+//                System.out.print(" ");
+//            }
+//            for(int k=1;k<=i;k++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//
+//        }
+//        for(int i=1;i<=4;i++){
+//            for(int j=1;j<=i;j++){
+//                System.out.print(" ");
+//            }
+//            for(int k=3;k>=i;k--){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+
+
+//        for(int i=1;i<=4;i++){
+//
+//            for(int j=4;j>=1;j--){
+//                if(j>i)
+//                    System.out.print(" ");
+//                else
+//                    System.out.print(" *");
+//            }
+//            System.out.println();
+//        }
+
+
+//        for(int i=1;i<=4;i++){
+//
+//            for(int j=4;j>=i;j--){
+//                System.out.print(" ");
+//            }
+//            for(int k=1;k<(i*2);k++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+
+        for(int i=5;i>=1;i--){
+
+            for(int j=5;j>i;j--){
                 System.out.print(" ");
             }
-            for(int k=1;k<=i;k++){
+            for(int k=1;k<(i*2);k++){
                 System.out.print("*");
             }
             System.out.println();
-
         }
-        for(int i=1;i<=4;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print(" ");
-            }
-            for(int k=3;k>=i;k--){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-
-
-
 
 
 
