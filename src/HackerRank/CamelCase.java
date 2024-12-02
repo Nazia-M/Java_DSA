1ed4cb290ecba7225c054f9658634bc10bc82560
Nazia <nazia862@gmail.com>
2024-12-02 12:28:19 +0530

    Commit 9 OCT 2024

--- src/HackerRank/CamelCase.java
+++ src/HackerRank/CamelCase.java
@@ -1,26 +1,47 @@
 package HackerRank;
 
+import java.util.List;
+
 public class CamelCase {
     public static void main(String[] args) {
-        String s = "oneTwoThree";
-
-        int count=0;
-        if(s.isEmpty() == false){
-
-            count =1;
-
-            for(int i=0;i<s.length();i++){
-
-                char ch = s.charAt(i);
-                if(ch >=65 && ch<=90){
-                    count++;
-                }
-
-            }
 
 
-        }
-        System.out.println(count);
+        //CamelcASE
+//        String s = "oneTwoThree";
+//
+//        int count=0;
+//        if(s.isEmpty() == false){
+//
+//            count =1;
+//
+//            for(int i=0;i<s.length();i++){
+//
+//                char ch = s.charAt(i);
+//                if(ch >=65 && ch<=90){
+//                    count++;
+//                }
+//
+//            }
+//
+//
+//        }
+//        System.out.println(count);
+
+
+        String str = "hackerrank";
+        String str1 = "haacckerannk";
+        int curr = 0;
+        for(int i = 0; i < str1.length(); i++)
+            if(curr < str.length() && str1.charAt(i) == str.charAt(curr))
+                curr++;
+        if(curr == str.length())
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+
+
+
 
     }
 }
+
