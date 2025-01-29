91c070f549e8227ccf1ecc54af1f30df4f369217
Nazia <nazia862@gmail.com>
2025-01-29 09:36:40 +0530

    Commit 9 OCT 2024

--- src/stringPrograms/StringManipulation.java
+++ src/stringPrograms/StringManipulation.java
@@ -6,6 +6,12 @@ public class StringManipulation {
 
         String str = "hi this is my java code and i am so happy";
 
+        //Get length of Stirng
+        System.out.println(str.length());
+
+        //Get character at 11
+        System.out.println(str.charAt(11));
+
         //First occurence
         System.out.println(str.indexOf('h'));
         System.out.println(str.indexOf('i'));
@@ -20,12 +26,25 @@ public class StringManipulation {
         System.out.println(str.indexOf("java"));
 
 
+        //Trim - Removes only starting and ending spaces not between spaces
+        String test = "     hello      world   ";
+        System.out.println(test.trim());
+
+        //Upper and Lowercase
+        String str1 = "all Not Small Letters";
+        System.out.println(str1.toUpperCase());
+        System.out.println(str1.toLowerCase());
+
+        //replace dob "12-08-1990"  to "12/08/1990"
+        String dob = "12-08-1990";
+        System.out.println(dob.replace("-", "/"));
+
+        //Replace all spaces with nothing
+        String str2 = "     hello      world   ";
+        System.out.println(str2.replace(" ",""));
+
 
 
 
     }
-
-
-
-
 }
