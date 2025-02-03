56ba192c0da7cdbc6c152e0113b73980abb2c3fe
Nazia <nazia862@gmail.com>
2025-02-03 09:55:38 +0530

    Commit 9 OCT 2024

--- src/stringPrograms/ReverseWords.java
+++ src/stringPrograms/ReverseWords.java
@@ -4,20 +4,20 @@ public class ReverseWords {
 
     public static void main(String[] args) {
 
-//        String str = "Java is very Easy";
-//
-//        String words[] = str.split("\\s");
-//
-//        String revStr = "";
-//
-//        for(int i=0;i<words.length;i++){
-//
-//            if(i == words.length-1)
-//                    revStr = words[i] + revStr;
-//            else
-//                revStr = " "+ words[i] + revStr;
-//        }
-//        System.out.println(revStr);
+        String str = "Java is very Easy";
+
+        String words[] = str.split("\\s");
+
+        String revStr = "";
+
+        for(int i=0;i<words.length;i++){
+
+            if(i == words.length-1)
+                    revStr = words[i] + revStr;
+            else
+                revStr = " "+ words[i] + revStr;
+        }
+        System.out.println(revStr);
 
         String str1 = "Java is very Easy";
         String words1[] = str1.split("\\s");
@@ -43,6 +43,35 @@ public class ReverseWords {
             System.out.println("-----------Reversed Word : "+rev+"  ---------");
         }
         System.out.println(rev);
+
+
+        int num = 54353;
+        String str0 = String.valueOf(num);
+        System.out.println("str "+str0);
+
+        String str11 = "354353";
+        int num1 = Integer.parseInt(str11);
+        System.out.println("num1 "+num1);
+
+        String str2 = "354353";
+        int num2 = Integer.valueOf(str2);
+        System.out.println("num2 "+num2);
+
+        String str3 = "Hello";
+        int num3 = Integer.valueOf(str3);
+        System.out.println("num3 "+num3);
+
+        try{
+            String str4 = "Hello";
+            int num4 = Integer.parseInt(str4);
+//            System.out.println("num4 "+num4);
+        }
+        catch (NumberFormatException e){
+            System.out.println("NumberFormatException");
+        }
+
+
+
 
 
 
